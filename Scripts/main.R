cb1d91e61572d80f63988c1e5655bf0f65d89f2e
thomas-legrand <thomas.legrand1@gmail.com>
2015-04-29 11:12:10 -0700

    Confirmed best perf, commiting the changes leading to it

--- Scripts/main.R
+++ Scripts/main.R
@@ -23,14 +23,6 @@ if(!exists("X.preproc") || kRecompute) {
   Xtest.preproc <- X.preproc[-c(1:ntrain), ]
 }
 
-CV(Xtrain.preproc, Ytrain, getModel(modelRF, ntree = 500, mtry = 2))
-
-CV(Xtrain.preproc, Ytrain, getModel(modelGBM, distribution = "tdist", n.trees = 1000, interaction.depth = 1, shrinkage = 0.01))
-
-if(!exists("Xtest.preproc") || kRecompute) {
-  Xtest.preproc <- preproc(Xtest)
-}
-
 launchCVs <- function(name, predictor) {
   predictor <<- predictor
   cat(name, ": ",
@@ -38,19 +30,19 @@ launchCVs <- function(name, predictor) {
       "\n", sep = "")
 }
 
-launchCVs(name = "RF", predictor = getModel(modelRF, ntree = 500, mtry = 2))
+#launchCVs(name = "RF", predictor = getModel(modelRF, ntree = 500, mtry = 2))
 
-launchCVs("GBM - 0.01", getModel(modelGBM, distribution = "tdist", n.trees = 1000, interaction.depth = 1, shrinkage = 0.01))
+#launchCVs("GBM - 0.01", getModel(modelGBM, distribution = "tdist", n.trees = 1000, interaction.depth = 1, shrinkage = 0.01))
 
-launchCVs("GBM - 0.1", getModel(modelGBM, distribution = "tdist", n.trees = 1000, interaction.depth = 1, shrinkage = 0.1))
+#launchCVs("GBM - 0.1", getModel(modelGBM, distribution = "tdist", n.trees = 1000, interaction.depth = 1, shrinkage = 0.1))
 
-launchCVs("SVM", getModel(modelSVM, cost = 2))
-launchCVs("SVM", getModel(modelRPART))
+#launchCVs("SVM", getModel(modelSVM, cost = 2))
+#launchCVs("SVM", getModel(modelRPART))
 
-launchCVs("Hybrid", getHybridModel(modelRF, modelSVM, cost = 2, ntree = 1000, mtry = 2))
+#launchCVs("Hybrid", getHybridModel(modelRF, modelSVM, cost = 2, ntree = 1000, mtry = 2))
 
-# writeSubmission(Xtest, getHybridModel(modelRF, modelSVM, cost = 2, ntree = 1000, mtry = 2)(preproc(Xtrain), Ytrain[,1], preproc(Xtest)), "HybridRFSVM-logpreproc")
-writeSubmission(Xtest, getModel(modelGBM, distribution = "tdist", n.trees = 1000, interaction.depth = 1, shrinkage = 0.01)(Xtrain.preproc, Ytrain[,1], Xtest.preproc), "HybridGBM-logpreproc")
+ writeSubmission(Xtest, getHybridModel(modelRF, modelSVM, cost = 2, ntree = 1000, mtry = 2)(preproc(Xtrain), Ytrain[,1], preproc(Xtest)), "HybridRFSVM-logpreproc")
+#writeSubmission(Xtest, getModel(modelGBM, distribution = "tdist", n.trees = 1000, interaction.depth = 1, shrinkage = 0.01)(Xtrain.preproc, Ytrain[,1], Xtest.preproc), "HybridGBM-logpreproc")
 # writeSubmission(Xtest, getModel(modelSVM, cost = 2)(Xtrain.preproc, Ytrain[,1], Xtest.preproc), "SVM-cost2")
 
 
--- Scripts/preprocessing.R
+++ Scripts/preprocessing.R
@@ -2,22 +2,23 @@
 
 # Features ideas
 preproc <- function(X){
-  library(mice)
-  obfuscated.cat.names <- paste0("P", c(5,6,7,8,11,15,22,24,33,37))
-  for (f in obfuscated.cat.names){
-    X[,f] <- as.factor(X[,f])
-  }
-  obfuscated.cat <- X[, colnames(X) %in% obfuscated.cat.names]
+  #library(mice)
+  #obfuscated.cat.names <- paste0("P", c(5,6,7,8,11,15,22,24,33,37))
+  #for (f in obfuscated.cat.names){
+  #  X[,f] <- as.factor(X[,f])
+  #}
+  #obfuscated.cat <- X[, colnames(X) %in% obfuscated.cat.names]
   X$Open.Date <- as.Date(X$Open.Date, format = "%m/%d/%Y")
   # Time since restaurant open
   age <- as.numeric(Sys.Date() - X$Open.Date, units = "days")
   isFCType <- factor(X$Type == "FC")
   isILType <- factor(X$Type == "IL")
-  obfuscated.num.names <- paste0("P", c(1:4,9,10,12:14,16:21,23,25:32,34:36))
-  obfuscated.num <- X[, colnames(X) %in% obfuscated.num.names]
-  obfuscated.num[obfuscated.num == 0] <- NA
-  imp <- mice(obfuscated.num, seed = 2015, m = 1, maxit = 3, method = "mean")
-  obfuscated.num <- complete(imp)
+  #obfuscated.num.names <- paste0("P", c(1:4,9,10,12:14,16:21,23,25:32,34:36))
+  #obfuscated.num <- X[, colnames(X) %in% obfuscated.num.names]
+  #obfuscated.num[obfuscated.num == 0] <- NA
+  #imp <- mice(obfuscated.num, seed = 2015, m = 1, maxit = 3, method = "mean")
+  #obfuscated.num <- complete(imp)
+  obfuscated.num <- obfuscated.num <- X[, colnames(X) %in% paste0("P", c(1:37))]
   log.obfuscated.num <- lapply(obfuscated.num, function(x) log(x+1))
   city.group <- factor(X$City.Group)
 
@@ -26,6 +27,6 @@ preproc <- function(X){
                     isFCType, 
                     isILType,
                     obfuscated.num,
-                    obfuscated.cat,
+                    #obfuscated.cat,
                     log.obfuscated.num))
 }
